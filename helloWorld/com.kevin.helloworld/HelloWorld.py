642e8b82e9b6820f50287d9ffdd946e57b972e98
白良锦 <bailiangjin@bailiangjindeMacBook-Pro-2.local>
2017-03-10 17:03:48 +0800

    提交

--- helloWorld/com.kevin.helloworld/HelloWorld.py
+++ helloWorld/com.kevin.helloworld/HelloWorld.py
@@ -1,1 +1,14 @@
-print 'Hello,World!'
+# print('Hello,World!')
+
+# x = 10
+# x = x + 2
+#
+# print x
+
+# name = input('please enter your name: ')
+
+# print('hello,', name)
+
+f = open('/Users/bailiangjin/dev/workspace/kevinswork/python/fileoperating/test.text', 'w')
+
+f.write("test.txtcontent")
